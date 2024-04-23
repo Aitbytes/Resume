3875990685423601dd7a205c9b00ef62086f11bd
aittaleb <aittaleb@insa-toulouse.fr>
2024-04-23 13:45:49 +0200

    Elaborate build script with english translation

--- generate.sh
+++ generate.sh
@@ -26,12 +26,22 @@ esac
 
 awesome_font_url="/home/a8taleb/Repos/resume/md_resume/markdown-resume/font-awesome-4.7.0/css/font-awesome.css"
 html_file_path="/home/a8taleb/Repos/resume/md_resume/markdown-resume/${output_html}"
+conversion_script_path="/home/a8taleb/Repos/resume/md_resume/markdown-resume/source/to_english.py"
 
 
 # Convert Markdown to HTML
 if ! pandoc "$input_markdown" --standalone --to html5 -o "$output_html"  --css "$css_file" --css "$awesome_font_url"; then
     echo "Error in Markdown to HTML conversion"
     exit 1
+fi
+
+if [[ $option == "en" ]]; then
+    if ! $conversion_script_path "$output_html"; then 
+        echo "Error in conversion to English"
+        exit 1
+    fi
+    output_html="modified.html"
+    html_file_path="/home/a8taleb/Repos/resume/md_resume/markdown-resume/${output_html}"
 fi
 
 # Convert HTML to PDF
