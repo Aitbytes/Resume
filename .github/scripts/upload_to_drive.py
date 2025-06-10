47979e36c7ffd7a48e65d330732a2cc4031d9855
a8taleb <a8taleb@nixos-hp>
2025-06-10 13:24:18 +0200

    adding drive_file_name to python script

--- .github/scripts/upload_to_drive.py
+++ .github/scripts/upload_to_drive.py
@@ -6,7 +6,7 @@ from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
 
-def upload_to_drive(file_path, folder_id, credentials_b64):
+def upload_to_drive(file_path, folder_id, credentials_b64, drive_file_name):
     # Decode the base64 encoded credentials
     credentials_json = base64.b64decode(credentials_b64).decode('utf-8')
     credentials_info = json.loads(credentials_json)
@@ -21,7 +21,7 @@ def upload_to_drive(file_path, folder_id, credentials_b64):
 
     # Prepare the file for upload
     file_metadata = {
-        'name': prefix + os.path.basename(file_path),
+        'name': prefix + drive_file_name,
         'parents': [folder_id]
     }
     media = MediaFileUpload(file_path, resumable=True)
@@ -38,6 +38,7 @@ def upload_to_drive(file_path, folder_id, credentials_b64):
 if __name__ == "__main__":
     file_path = os.getenv("FILE_PATH")
     folder_id = os.getenv("FOLDER_ID")
+    drive_file_name = os.getenv("DRIVE_FILE_NAME")
     credentials_b64 = os.getenv("GOOGLE_CREDENTIALS")
 
-    upload_to_drive(file_path, folder_id, credentials_b64)
+    upload_to_drive(file_path, folder_id, credentials_b64, drive_file_name)
